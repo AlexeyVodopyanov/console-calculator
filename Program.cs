81a262199c489c83f705be568ad9f3d111b75b8e
Алексей <52876090+AlexeyVodopyanov@users.noreply.github.com>
2020-11-09 14:10:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+namespace Calc
+{
+    class Program
+    {
+        
+
+        public static int GetResult(int a, int b, string cmd)
+        {
+            int result;
+            switch (cmd)
+            {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+            return result;
+        }
+        static void Main()
+        {
+            WriteLine("Введите первое число ");
+            int a = Convert.ToInt32(ReadLine());
+            WriteLine("Введите второе число ");
+            int b = Convert.ToInt32(ReadLine());
+
+            WriteLine("Пожалуйста выбирите +, -, *, / ");
+            string cmd = ReadLine();
+            int result = GetResult(a, b, cmd);
+            WriteLine($"{a} {cmd} {b} = {result}");
+        }
+    }
+}
